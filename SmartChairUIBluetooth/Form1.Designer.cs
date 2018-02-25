532665489d0e5f0e5da1129f07f40c36b5cea2ee
ncolford <colford.nathan@gmail.com>
2018-02-25 15:01:44 -0400

    ???

---
+++ SmartChairUIBluetooth/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace SmartChairUIBluetooth
+{
+    partial class Smr_Chair
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.BluetoothMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.BluetoothMenuConnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.BluetoothMenuDisconnect = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.User_Input = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BluetoothMenu,
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(787, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // BluetoothMenu
+            // 
+            this.BluetoothMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BluetoothMenuConnect,
+            this.BluetoothMenuDisconnect,
+            this.exitToolStripMenuItem});
+            this.BluetoothMenu.Name = "BluetoothMenu";
+            this.BluetoothMenu.Size = new System.Drawing.Size(71, 20);
+            this.BluetoothMenu.Text = "Bluetooth";
+            this.BluetoothMenu.Click += new System.EventHandler(this.bluetoothMenu_Click);
+            // 
+            // BluetoothMenuConnect
+            // 
+            this.BluetoothMenuConnect.Name = "BluetoothMenuConnect";
+            this.BluetoothMenuConnect.Size = new System.Drawing.Size(171, 22);
+            this.BluetoothMenuConnect.Text = "Connect to Device";
+            this.BluetoothMenuConnect.Click += new System.EventHandler(this.BluetoothMenuConnect_Click);
+            // 
+            // BluetoothMenuDisconnect
+            // 
+            this.BluetoothMenuDisconnect.Name = "BluetoothMenuDisconnect";
+            this.BluetoothMenuDisconnect.Size = new System.Drawing.Size(171, 22);
+            this.BluetoothMenuDisconnect.Text = "Disconeect";
+            this.BluetoothMenuDisconnect.Click += new System.EventHandler(this.BluetoothMenuDisconnect_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(573, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(573, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(172, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(573, 159);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(172, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(573, 206);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(172, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(573, 262);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(172, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(573, 313);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(172, 20);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(573, 364);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(172, 20);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(573, 419);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(172, 20);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(573, 477);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(172, 20);
+            this.textBox10.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ENTER COM PORT:";
+            // 
+            // User_Input
+            // 
+            this.User_Input.Location = new System.Drawing.Point(173, 139);
+            this.User_Input.Name = "User_Input";
+            this.User_Input.Size = new System.Drawing.Size(100, 20);
+            this.User_Input.TabIndex = 12;
+            this.User_Input.TextChanged += new System.EventHandler(this.User_Input_TextChanged);
+            // 
+            // Smr_Chair
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 694);
+            this.Controls.Add(this.User_Input);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Smr_Chair";
+            this.Text = "Smart Chair Control Center";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem BluetoothMenu;
+        private System.Windows.Forms.ToolStripMenuItem BluetoothMenuConnect;
+        private System.Windows.Forms.ToolStripMenuItem BluetoothMenuDisconnect;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox User_Input;
+    }
+}
+
---
+++ SmartChairUIBluetooth/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmChair_Functions;
+
+namespace SmartChairUIBluetooth
+{
+    public partial class Smr_Chair : Form
+    {
+
+        //some variables
+        SmChairFunc Smfunc = new SmChairFunc();
+        private float[] heatmap_sens = new float[9];
+        private float temp_val;
+        private string temp_string;
+        private string display_val;
+        private string com_port;
+        private Time_Data TData = new Time_Data(50);
+
+        public Smr_Chair()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bluetoothMenu_Click(object sender, EventArgs e)
+        {
+            //test
+        }
+
+        private void BluetoothMenuConnect_Click(object sender, EventArgs e)
+        {
+            //Connect should connect to the bluetooth device (the MCU), currently, you have to find
+            //which com port that is by yourself and change it below
+            serialPort1.PortName = com_port;
+
+            //Now we configure the port
+            serialPort1.BaudRate = 9600;
+            serialPort1.Parity = System.IO.Ports.Parity.None;
+            serialPort1.StopBits = System.IO.Ports.StopBits.One;
+            serialPort1.Handshake = System.IO.Ports.Handshake.None;
+
+            serialPort1.Open();
+            serialPort1.ReadTimeout = 1000000; //reading timeouts in 100 milliseconds, later this should be no timeout
+
+            //handle the recieve data event for our serial port (how we store data basically)
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(serialPort1_DataRecieved);
+        }
+
+        private void serialPort1_DataRecieved(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            //Store the data recieved through serial port
+            //Notation D - data, 1 - 9 which sensor
+            //example: D53.33 is sending a reading from sensor 5 of 3.33 volts
+            display_val = serialPort1.ReadExisting();
+
+            //processing the data
+            if (display_val[0] == 'D')
+            {
+                //finds floats in recieved data.
+                temp_string = display_val.Substring(2);
+                temp_val = Convert.ToSingle(temp_string);
+                switch (display_val[1])
+                {
+                    case '1':
+                        heatmap_sens[0] = temp_val;
+                        break;
+                    case '2':
+                        heatmap_sens[1] = temp_val;
+                        break;
+                    case '3':
+                        heatmap_sens[2] = temp_val;
+                        break;
+                    case '4':
+                        heatmap_sens[3] = temp_val;
+                        break;
+                    case '5':
+                        heatmap_sens[4] = temp_val;
+                        break;
+                    case '6':
+                        heatmap_sens[5] = temp_val;
+                        break;
+                    case '7':
+                        heatmap_sens[6] = temp_val;
+                        break;
+                    case '8':
+                        heatmap_sens[7] = temp_val;
+                        break;
+                    case '9':
+                        heatmap_sens[8] = temp_val;
+                        break;
+                    default:
+                        display_val = "INVALID DATA";
+                        break;
+                }
+
+            }
+
+            //call a seperate event to handle recieved data
+            this.Invoke(new EventHandler(DisplayText));
+        }
+
+        private void DisplayText(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            textBox9.Clear();
+            textBox10.Clear();
+
+            textBox1.AppendText(display_val);
+            textBox2.AppendText(Convert.ToString(heatmap_sens[0]));
+            textBox3.AppendText(Convert.ToString(heatmap_sens[1]));
+            textBox4.AppendText(Convert.ToString(heatmap_sens[2]));
+            textBox5.AppendText(Convert.ToString(heatmap_sens[3]));
+            textBox6.AppendText(Convert.ToString(heatmap_sens[4]));
+            textBox7.AppendText(Convert.ToString(heatmap_sens[5]));
+            textBox8.AppendText(Convert.ToString(heatmap_sens[6]));
+            textBox9.AppendText(Convert.ToString(heatmap_sens[7]));
+            textBox10.AppendText(Convert.ToString(heatmap_sens[8]));
+
+        }
+
+        private void BluetoothMenuDisconnect_Click(object sender, EventArgs e)
+        {
+            //Clicking Disconnect will close COM5, make sure to do this when you're done with it
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)    //Quit the program
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void User_Input_TextChanged(object sender, EventArgs e)
+        {
+            com_port = User_Input.Text;
+        }
+    }
+}
